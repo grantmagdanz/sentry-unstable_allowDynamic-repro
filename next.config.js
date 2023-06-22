ea82303a0507647ff6e890f84586a0f6fa0a3ce8
Grant Magdanz <grant@gm.xyz>
2023-06-22 08:30:02 -0600

    Setup sentry

--- next.config.js
+++ next.config.js
@@ -1,6 +1,49 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
+// Toggle this to repro error
+const skipSentry = false
+
 module.exports = nextConfig
+
+if (skipSentry) {
+  module.exports = nextConfig
+} else {
+  // Injected content via Sentry wizard below
+  module.exports = withSentryConfig(
+    nextConfig,
+    {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
+
+      // Suppresses source map uploading logs during build
+      silent: true,
+
+      org: "",
+      project: "",
+    },
+    {
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
+
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
+
+      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+      tunnelRoute: "/monitoring",
+
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
+
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+    }
+  )
+}
